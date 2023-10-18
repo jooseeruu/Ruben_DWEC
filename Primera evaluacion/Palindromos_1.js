b69bf55286c958aa57282d38ceeb0150e5880d5c
Jooseeruu <arjona2908@gmail.com>
2023-10-18 16:48:49 +0200

    Pretty code

--- Primera evaluacion/Palindromos_1.js
+++ Primera evaluacion/Palindromos_1.js
@@ -4,12 +4,11 @@ var palabraReversa = "";
 palabra = palabra.toLowerCase();
 
 for (var i = palabra.length - 1; i >= 0; i--) {
-    palabraReversa += palabra[i];
+  palabraReversa += palabra[i];
 }
 
 if (palabra === palabraReversa) {
-    console.log(palabra+" es un palíndromo.");
+  console.log(palabra + " es un palíndromo.");
 } else {
-    console.log(palabra+" no es un palíndromo.");
+  console.log(palabra + " no es un palíndromo.");
 }
-
--- Primera evaluacion/Palindromos_3.js
+++ Primera evaluacion/Palindromos_3.js
@@ -5,17 +5,27 @@ var palabraSinAcentos = "";
 palabra = palabra.toLowerCase();
 
 for (var i = 0; i < palabra.length; i++) {
-    if (palabra[i] !== ' ') {
-        var letra = palabra[i];
-        switch(letra) {
-            case 'á': letra = 'a'; break;
-            case 'é': letra = 'e'; break;
-            case 'í': letra = 'i'; break;
-            case 'ó': letra = 'o'; break;
-            case 'ú': letra = 'u'; break;
-        }
-        palabraSinEspacios += letra;
+  if (palabra[i] !== " ") {
+    var letra = palabra[i];
+    switch (letra) {
+      case "á":
+        letra = "a";
+        break;
+      case "é":
+        letra = "e";
+        break;
+      case "í":
+        letra = "i";
+        break;
+      case "ó":
+        letra = "o";
+        break;
+      case "ú":
+        letra = "u";
+        break;
     }
+    palabraSinEspacios += letra;
+  }
 }
 
 palabraSinAcentos = palabraSinEspacios;
@@ -23,11 +33,11 @@ palabraSinAcentos = palabraSinEspacios;
 var palabraReversa = "";
 
 for (var i = palabraSinAcentos.length - 1; i >= 0; i--) {
-    palabraReversa += palabraSinAcentos[i];
+  palabraReversa += palabraSinAcentos[i];
 }
 
 if (palabraSinAcentos === palabraReversa) {
-    console.log(palabra+" es un palíndromo.");
+  console.log(palabra + " es un palíndromo.");
 } else {
-    console.log(palabra+" no es un palíndromo.");
+  console.log(palabra + " no es un palíndromo.");
 }
