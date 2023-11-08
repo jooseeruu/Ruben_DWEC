d31a8f3bf0b82ff28826227eca81101c637ff2b6
Jooseeruu <arjona2908@gmail.com>
2023-11-08 17:24:20 +0100

    Veterinario

---
+++ Veterinario/model/Animal.js
@@ -0,0 +1,7 @@
+export function Animal(id,name,sex,registerynumber,type){
+    this.id = id;
+    this.name = name;
+    this.sex = sex;
+    this.registerynumber = registerynumber;
+    this.type = type;
+}
