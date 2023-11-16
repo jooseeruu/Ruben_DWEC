4e9572e55dd1abceb8731500162bdc47997e79f2
Jooseeruu <arjona2908@gmail.com>
2023-11-16 18:10:53 +0100

    Arreglos y mejoras

--- Primera evaluacion/caracruz.js
+++
@@ -1,6 +0,0 @@
-function caracruz(){ 
-    var User = 1; 
-    var Moneda_al_aire= Math.floor(Math.random() * 2) + 1;
-     if (User===Moneda_al_aire) { console.log("Has ganado");}
-      else { console.log("Has perdido"); } } 
-caracruz();
